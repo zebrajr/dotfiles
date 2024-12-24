a3254582519d92737a633261b32a4e57c4c2f218
Carlos Sousa <me@carlossousa.tech>
2024-12-24 16:18:51 +0100

    update harpoon keybinds, undotree visuals

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -17,6 +17,22 @@ return {
            harpoon.ui:toggle_quick_menu(harpoon:list())
         end, { desc = '[H]arpoon [Q]uickmenu' })
 
+        -- hotkeys for top 4 items
+        vim.keymap.set('n', '<leader>h1', function()
+            harpoon:list():select(1)
+        end, { desc = '[H]arpoon File [1]' })
+
+        vim.keymap.set('n', '<leader>h2', function()
+            harpoon:list():select(2)
+        end, { desc = '[H]arpoon File [2]' })
+
+        vim.keymap.set('n', '<leader>h3', function()
+            harpoon:list():select(3)
+        end, { desc = '[H]arpoon File [3]' })
+        vim.keymap.set('n', '<leader>h4', function()
+            harpoon:list():select(4)
+        end, { desc = '[H]arpoon File [4]' })
+
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set('n', '<C-PageDown>', function() harpoon:list():prev() end)
         vim.keymap.set('n', '<C-PageUp>', function() harpoon:list():next() end)
--- config/nvim/lua/plugins/undotree.lua
+++ config/nvim/lua/plugins/undotree.lua
@@ -3,5 +3,8 @@ return {
 
     config = function()
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end
+
+        vim.g.undotree_WindowLayout = 2
+        vim.g.undotree_DiffpanelHeight = 20
+    end,
 }
