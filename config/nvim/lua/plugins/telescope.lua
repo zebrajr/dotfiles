7505e2182b5abace233c5ec998f0aa3e3a8d5890
Carlos Sousa <me@carlossousa.tech>
2025-01-07 16:22:51 +0100

    add live grep

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -23,9 +23,11 @@ return {
 
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
         vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[F]ind in [g]it files' })
-        vim.keymap.set('n', '<leader>fG', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end, { desc = '[F]ind via [G]rep' })
+        vim.keymap.set('n', '<leader>fG', builtin.live_grep, { desc = '[F]ind via live [G]rep' })
+        vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = '[F]ind grep [s]tring' })
+        -- vim.keymap.set('n', '<leader>fG', function()
+        --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        -- end, { desc = '[F]ind via [G]rep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind in [B]uffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind in [H]elp Tags' })
     end,
