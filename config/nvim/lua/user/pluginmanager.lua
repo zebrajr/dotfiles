fa346ec5592c65e75edfc51b9e0c5cbac2a4a096
Carlos Sousa <me@carlossousa.tech>
2025-01-07 21:50:08 +0100

    removed plugin manager notifications

--- config/nvim/lua/user/pluginmanager.lua
+++ config/nvim/lua/user/pluginmanager.lua
@@ -26,9 +26,16 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = {
-      colorscheme = { "tokyonight-storm" }
+      missing = true,
+      colorscheme = { "tokyonight-storm" },
       },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+      enabled = true,
+      notify = false,
+  },
+  change_detection = {
+      notify = false,
+  },
 })
 
